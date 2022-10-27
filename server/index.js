04521911d113ef48bf2fdb623f9d88ad00e65701
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-10-27 05:37:49 +0000

    forgot to push client folder

--- server/index.js
+++ server/index.js
@@ -16,19 +16,25 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  socket.emit("hello", "world");
+  
+  socket.on("connect", () => {
+    socket.emit("hello", "world");
   });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+  // socket.on("join_room", (data) => {
+  //   socket.join(data);
+  //   console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  // });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
+  // socket.on("send_message", (data) => {
+  //   socket.to(data.room).emit("receive_message", data);
+  // });
+
+  // socket.on("disconnect", () => {
+  //   console.log("User Disconnected", socket.id);
+  // });
+
 });
 
 server.listen(3001, () => {
